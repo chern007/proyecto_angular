684c31538c312041659c75ad58e5d21c26bdb8cb
carlos <carloshernandezcrespo@gmail.com>
2020-04-17 20:23:48 +0200

    39 OK

--- platzinger/src/app/login/login.component.ts
+++ platzinger/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -8,10 +9,52 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   operation: string = 'login';
+  email: string = null;
+  password: string = null;
 
-  constructor() { }
+  constructor(private authenticationService: AuthenticationService) { }
 
   ngOnInit(): void {
+  }
+
+  //funcion de logado de usuario
+  login() {
+
+    this.authenticationService.loginWithEmail(this.email, this.password).then(
+
+      (data) => {
+
+        alert('logado correctamen5te');
+        console.log(data);
+
+      }
+
+    ).catch(error => {
+
+      console.log(error);
+
+    });
+
+  }
+
+  //funcion de registro de nuevo usuario
+  register() {
+
+    this.authenticationService.registerWithEmail(this.email, this.password).then(
+
+      (data) => {
+
+        alert('registrado correctamente');
+        console.log(data);
+
+      }
+
+    ).catch(error => {
+
+      console.log(error);
+
+    });
+
   }
 
 }
