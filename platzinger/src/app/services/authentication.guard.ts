077baf155125847281119f1b8d7cfaa751a0f58e
carlos <carloshernandezcrespo@gmail.com>
2020-04-30 12:43:30 +0200

    Explicación de Observable en Authentication.Guard

--- platzinger/src/app/services/authentication.guard.ts
+++ platzinger/src/app/services/authentication.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router, RouteReuseStrategy } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthenticationService } from './authentication.service';
 import { map } from 'rxjs/operators'
@@ -9,14 +9,20 @@ import { map } from 'rxjs/operators'
 })
 export class AuthenticationGuard implements CanActivate {
 
-  constructor(private authenticationService: AuthenticationService, private router : Router) { };
+  constructor(private authenticationService: AuthenticationService, private router: Router) { };
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return this.authenticationService.getStatus().pipe(
+
+
+
+    var resultObservable : Observable<boolean> = this.authenticationService.getStatus().pipe( //--> Observable<boolean>
 
       map(status => {
+
+        console.log("IMPRIMIMOS EL STATUS DE LA AUTENTIFICACION: ");
+        console.log(status);
 
         if (status) {
 
@@ -33,6 +39,11 @@ export class AuthenticationGuard implements CanActivate {
       })
 
     );
+
+
+    return resultObservable;
+
+
   }
 
 }
